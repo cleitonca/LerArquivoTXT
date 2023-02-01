ee58233da0a45747de97fad1cff70e1f0e3c25fe
Cleiton Araújo <cltn.info@gmail.com>
2023-02-01 14:22:48 -0300

    Projeto Ler Arquivo TXT

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+var caminho = new StreamReader(@"D:\Estudo de Desenvolvimento\CSharp\LerArquivoTXT\Arquivo.txt");
+
+using (caminho)
+{
+    while (!caminho.EndOfStream)
+    {
+        var linhaCaminho = caminho.ReadLine();
+        Console.WriteLine(linhaCaminho);
+    }
+}
+
+
